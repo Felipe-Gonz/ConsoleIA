73d58c401be6ea06445df06d175b8190bf18024b
Felipe-Gonz <fg180899@gmail.com>
2026-03-09 23:14:31 -0700

    Commit init

---
+++ src/main/java/com/luisgonzalez/ConsoleIA/ConsoleIaApplication.java
@@ -0,0 +1,13 @@
+package com.luisgonzalez.ConsoleIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsoleIaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisgonzalez/ConsoleIA/ConsoleIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisgonzalez.ConsoleIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsoleIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
